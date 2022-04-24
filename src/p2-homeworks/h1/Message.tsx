3f1bcd82e61c307bbd6b1795a717181c1179a3b5
KobraMik <slavik433@gmail.com>
2022-04-24 18:43:00 +0300

    first task completed

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import style from './Message.module.css'
 
-function Message() {
+
+type propsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: propsType) {
     return (
-        <div>
+        <div className={style.message}>
+            <img src={props.avatar} className={style.avatar}/>
 
+            <div className={style.angle}/>
+
+            <div className={style.data}>
+                <div className={style.name}>{props.name}</div>
+                <div className={style.text}>{props.message}</div>
+                <div className={style.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
