63034c822dce4e6e1eb985dc17e38c37ebc229af
Mike <slavik433@gmail.com>
2022-04-27 16:00:55 +0300

    task 5 completed (Routes)

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import s from './pages/Pages.module.css'
+import {NavLink} from 'react-router-dom'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <div className={s.nav}><NavLink to='/pre-junior'
+                                            className={({isActive}) => isActive ? s.active : s.nav}>preJun</NavLink>
+            </div>
+            <div className={s.nav}><NavLink to='/Junior'
+                                            className={({isActive}) => isActive ? s.active : s.nav}>Jun</NavLink>
+            </div>
+            <div className={s.nav}><NavLink to='/junior-plus'
+                                            className={({isActive}) => isActive ? s.active : s.nav}>JuniorPlus</NavLink>
+            </div>
+            <div className={s.block}/>
         </div>
     )
 }
